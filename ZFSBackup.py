cf0086cd4be63d0e241e342dc39dc16baca9daaf
Sean Eric Fagan <sef@kithrup.com>
2017-06-28 16:48:41 -0700

    Set the storage class for the uploaded chunks to STANDARD_IA (infrequently accessed).  This is a bit cheaper with S3.

--- ZFSBackup.py
+++ ZFSBackup.py
@@ -1241,6 +1241,7 @@ class ZFSBackupS3(ZFSBackupDirectory):
             total = 0
             uploader = self.s3.create_multipart_upload(Bucket=self.bucket,
                                                        ACL='private',
+                                                       StorageClass='STANDARD_IA',
                                                        Key=chunk_key)
             upload_id = uploader['UploadId']
             parts = []
